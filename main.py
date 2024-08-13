34be2ba847c2381720feda380de3794d8384cd5b
Dmytro Oliinyk <devdm.dp@gmail.com>
2024-08-13 20:39:58 +0000

    add category del\add

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from handlers.add_item import add_item
 from handlers.done import done
 from handlers.view_list import view_list
 from handlers.remove_item import remove_item
+from handlers.add_category import add_category
+from handlers.remove_category import remove_category
 
 
 def main() -> None:
     updater = Updater("YOUR TELEGRAM BOT TOKEN")
 
+    # Існуючі обробники
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('done', done))
@@ -17,6 +20,11 @@ def main() -> None:
     updater.dispatcher.add_handler(MessageHandler(
         Filters.text & ~Filters.command, add_item))
     updater.dispatcher.add_handler(CommandHandler('remove', remove_item))
+
+    # Нові обробники для категорій
+    updater.dispatcher.add_handler(CommandHandler('addcategory', add_category))
+    updater.dispatcher.add_handler(
+        CommandHandler('removecategory', remove_category))
 
     updater.start_polling()
     updater.idle()
